98d943478e751381b907cf984d9a6546be02722b
Jordfoz16 <jordfoz@gmail.com>
2019-02-19 16:18:28 +0000

    Initial Commit

---
+++ Silience/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Silience
+//
+//  Created by Jordan Foster on 19/02/2019.
+//  Copyright © 2019 Silience. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Silience/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Silience
+//
+//  Created by Jordan Foster on 19/02/2019.
+//  Copyright © 2019 Silience. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
