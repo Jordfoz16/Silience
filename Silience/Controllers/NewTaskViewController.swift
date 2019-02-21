be9c7b6ea04b8ef0074b9b7d216e89dda42a0400
Jordfoz16 <jordfoz@gmail.com>
2019-02-21 14:20:13 +0000

    Fixed Date input

--- Silience/Controllers/NewTaskViewController.swift
+++ Silience/Controllers/NewTaskViewController.swift
@@ -15,16 +15,42 @@ class NewTaskViewController: UIViewController {
     @IBOutlet weak var startDate: UITextField!
     @IBOutlet weak var endDate: UITextField!
     
+    private var datePicker: UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(dateChange(datePicker:)), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped(gestrueRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        startDate.inputView = datePicker
+        endDate.inputView = datePicker
         nameTextBox.becomeFirstResponder()
+    }
+    
+    @objc func dateChange(datePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        if(startDate.isEditing){
+            startDate.text = dateFormatter.string(from: datePicker.date)
+        }else if(endDate.isEditing){
+            endDate.text = dateFormatter.string(from: datePicker.date)
+        }
+    }
+    
+    @objc func viewTapped(gestrueRecognizer: UITapGestureRecognizer){
+        view.endEditing(true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         nameTextBox.resignFirstResponder()
         timeTextBox.resignFirstResponder()
-        startDate.resignFirstResponder()
-        endDate.resignFirstResponder()
+        
     }
 }
