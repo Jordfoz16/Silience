37367502713590d94458551fe98558601b4fce02
Jordfoz16 <jordfoz@gmail.com>
2019-02-27 10:47:13 +0000

    File Managment and JSON Parsing

--- Silience/Controllers/NewTaskViewController.swift
+++ Silience/Controllers/NewTaskViewController.swift
@@ -12,39 +12,51 @@ class NewTaskViewController: UIViewController {
     
     @IBOutlet weak var nameTextBox: UITextField!
     @IBOutlet weak var timeTextBox: UITextField!
-    @IBOutlet weak var startDate: UITextField!
-    @IBOutlet weak var endDate: UITextField!
+    @IBOutlet weak var startDateTextBox: UITextField!
+    @IBOutlet weak var endDateTextBox: UITextField!
+    @IBOutlet weak var minutesLabel: UILabel!
     
     private var datePicker: UIDatePicker?
+    private var startDate: Date = Date.init()
+    private var endDate: Date = Date.init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Date Picker Setup for the date input fields
         datePicker = UIDatePicker()
         datePicker?.datePickerMode = .date
         datePicker?.addTarget(self, action: #selector(dateChange(datePicker:)), for: .valueChanged)
         
+        //Tap gesture to close the datepicker when date is selected
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped(gestrueRecognizer:)))
-        
         view.addGestureRecognizer(tapGesture)
         
-        startDate.inputView = datePicker
-        endDate.inputView = datePicker
+        //Setting the inputs to the date picker
+        startDateTextBox.inputView = datePicker
+        endDateTextBox.inputView = datePicker
+        
+        //Making the keyboard to show
         nameTextBox.becomeFirstResponder()
     }
     
     @objc func dateChange(datePicker: UIDatePicker){
+        //Formatting the date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy"
         
-        if(startDate.isEditing){
-            startDate.text = dateFormatter.string(from: datePicker.date)
-        }else if(endDate.isEditing){
-            endDate.text = dateFormatter.string(from: datePicker.date)
+        //Changing the correct date textbox
+        if(startDateTextBox.isEditing){
+            startDate = datePicker.date
+            startDateTextBox.text = dateFormatter.string(from: datePicker.date)
+        }else if(endDateTextBox.isEditing){
+            endDate = datePicker.date
+            endDateTextBox.text = dateFormatter.string(from: datePicker.date)
         }
     }
     
     @objc func viewTapped(gestrueRecognizer: UITapGestureRecognizer){
+        //Close the datepicker when the tap gesture
         view.endEditing(true)
     }
     
@@ -52,5 +64,56 @@ class NewTaskViewController: UIViewController {
         nameTextBox.resignFirstResponder()
         timeTextBox.resignFirstResponder()
         
+    }
+    
+    @IBAction func changedHours(sender: Any) {
+        updateWorkLoad()
+    }
+    
+    @IBAction func changedStartDate(sender: Any) {
+        updateWorkLoad()
+    }
+    
+    @IBAction func changedEndDate(sender: Any) {
+        updateWorkLoad()
+    }
+    
+    func updateWorkLoad(){
+        //Check data has been entered
+        if(timeTextBox.text == "" || startDateTextBox.text == "" || endDateTextBox.text == ""){
+            return
+        }
+        let hours:Double = Double(timeTextBox.text!)!
+        
+        //Amount of days between the dates
+        
+        if(endDate != startDate){
+            let dayGap = (endDate.timeIntervalSince(startDate)) / 60 / 60 / 24
+            let timePerDay = Int((hours/dayGap)*60)
+            
+            minutesLabel.text = String(timePerDay) + " min/day"
+        }else{
+            minutesLabel.text = "N/a"
+        }
+        
+    }
+    
+    @IBAction func addNewTask(sender: Any) {
+        
+        let fileManager = File()
+        let jsonParser = ParseJSON()
+        
+        let name = nameTextBox.text
+        let startDate = startDateTextBox.text
+        let endDate = endDateTextBox.text
+        let hours = timeTextBox.text
+        let description = "None"
+        
+        let newTask: Task = Task(name: name!, startDate: startDate!, endDate: endDate!, hours: hours!, description: description)
+        
+        jsonParser.addTask(task: newTask)
+        jsonParser.encodeJSON()
+        
+        fileManager.writeFile(writeString: jsonParser.jsonToString(), fileName: "Tasks", fileExtension: "json")
     }
 }
